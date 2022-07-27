840ddd2c97024e052e22cda4dd53422ab566e301
Sahandgha <sahand.gha@gmail.com>
2022-07-27 10:19:32 +1000

    complete the oauth functionality

--- components/OAuth.js
+++ components/OAuth.js
@@ -1,8 +1,42 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { useRouter } from "next/router";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
 export default function OAuth() {
+  const router = useRouter();
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      // check for the user
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      router.push("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+      console.log(error);
+    }
+  }
   return (
-    <button className="mt-6 flex items-center justify-center w-full bg-red-700 text-white py-3 px-7 uppercase text-sm font-medium rounded shadow-md hover:bg-red-800 hover:shadow-lg active:bg-red-900 active:shadow-lg transition duration-150 ease-in-out">
-      <FcGoogle className="text-2xl bg-white border rounded-full mr-2" /> Continue with Google
+    <button
+      type="button"
+      onClick={onGoogleClick}
+      className="mt-6 flex items-center justify-center w-full bg-red-700 text-white py-3 px-7 uppercase text-sm font-medium rounded shadow-md hover:bg-red-800 hover:shadow-lg active:bg-red-900 active:shadow-lg transition duration-150 ease-in-out"
+    >
+      <FcGoogle className="text-2xl bg-white border rounded-full mr-2" />{" "}
+      Continue with Google
     </button>
   );
 }
